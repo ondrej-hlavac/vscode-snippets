e6f99f5f2273f61eb992c352e200c270ce329e49
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2020-12-11 03:27:12 +0100

    Test file

---
+++ test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface IProps {}
+
+const test = ({}: IProps) => {
+  return <></>;
+};
+
+export default test;
+
+import React from "react";
+
+const test = () => {
+  return <></>;
+};
+
+export default test;
